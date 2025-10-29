19145c0bf073a5fc4962078dddb81a6642858a8f
Victor Batista <batistajvictor@gmail.com>
2025-10-29 16:49:47 -0300

    feat/atualização do código para baixar e concatenar a base da pnad

--- data/domicilios.R
+++ data/domicilios.R
@@ -1,13 +1,23 @@
 library(PNADcIBGE)
 library(survey)
+library(dplyr)
+library(purrr)
 
+# Função atualizada para baixar e processar
 baixar_base <- function(ano) {
-  nome_objeto <- paste0("dados_", ano)
   dados <- get_pnadc(year = ano, interview = 1, design = FALSE)
-  assign(nome_objeto, dados, envir = .GlobalEnv)
-  return(invisible(dados))
+  
+  # Selecionar colunas e adicionar ano
+  dados_processados <- dados %>%
+    select(Ano, UF, Capital, RM_RIDE, V1022, V1023, V1031, starts_with("S01"))
+  
+  return(dados_processados)
 }
 
-for (ano in 2016:2024){
-  baixar_base(ano)
-}
+# Aplicar a função para todos os anos e concatenar
+painel_completo <- 2016:2024 %>%
+  map_df(baixar_base)
+
+# Visualizar resultado
+cat("Dimensões do painel final:", nrow(painel_completo), "linhas x", ncol(painel_completo), "colunas\n")
+head(painel_completo)
